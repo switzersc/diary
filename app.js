845674f1ae9ea0c2339b98c3a896abbd20bcc2c5
Aaron <hellometa@Aarons-MacBook-Pro.local>
2015-11-07 20:07:46 -0700

    Include MVP client code.

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
+var formData = {}
+
 document.addEventListener('DOMContentLoaded', function() {
-  registerEvents()
+  getCurrentUrl()
+    .then(buildData)
+  
+  var saveButton = document.querySelector('[data-event="saveEvent"]')
+  
+  saveButton.addEventListener('click', function(e) {
+    var data = formatData(formData)
+    postData(data)
+  })
 })
 
-function postEvent() {
-  // send event data to Cal API
+function buildData(url) {
+  formData.url = url
 }
 
-function registerEvents() {
-  var saveButton = document.querySelector('[data-event="saveEvent"]')
-  saveButton.addEventListener('click', function() {
-    console.log('clicked')
-  })
+function formatData(obj) {
+  var data = {
+    title: document.getElementById('title').value,
+    data: document.getElementById('date').value,
+    time: document.getElementById('time').value,
+    url: obj.url
+  }
+  return data
 }
+
+function postData(data) {
+  console.log(data)
+}
+
+function getCurrentUrl() {
+  var promise = new Promise(function(resolve, reject) {
+    var queryInfo = {
+      active: true,
+      currentWindow: true
+    }
+    
+    chrome.tabs.query(queryInfo, function(tabs) {
+      var tab = tabs[0]
+      var url = tab.url
+      resolve(url)
+    })
+  })
+  return promise
+}
+
